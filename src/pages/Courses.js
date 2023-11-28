a22caab485a9c012ed434eb9ffc3e1cb4d21e19b
Owusu-Desmond <desmond0406114009@gmail.com>
2023-11-28 03:10:43 +0000

    Complete course page

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,14 +1,83 @@
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import Header from '../components/Hearder';
+import '../css/Courses.css';
+import WOW from 'wowjs';
+import Footer from '../components/Footer';
+import Subjects from '../components/Subjects';
+
+// import header image
+import bg from '../assets/images/header_images/courses_header_image.jpeg';
+
+// import subjects images
+import CyberSecurity from '../assets/images/subjects_images/Cyber Security.jpg';
+import WebProgramming from '../assets/images/subjects_images/Web Dev.png';
+import MAD from '../assets/images/subjects_images/Mobile App Dev.jpg';
+import CNF from '../assets/images/subjects_images/CCNA.png';
 
 const Courses = () => {
 
+  useEffect(() => {
+    const wow = new WOW.WOW();
+    wow.init();
+  }, []);
+
+  const coursesData = [
+    {
+      imgSrc: CyberSecurity,
+      title: 'Cyber Security',
+      description: 'Learn the various techniques used to secure a computer system from attacks through hacking.',
+      link: '/courses/'
+    },
+    {
+      imgSrc: WebProgramming,
+      title: 'Web Programming',
+      description: 'Understand the fundamentals of web technologies and create web-based applications with latest technologies.',
+      link: '/courses/'
+    },
+    {
+      imgSrc: MAD,
+      title: 'Mobile App Development',
+      description: 'We provide world class technologies for Mobile Application Development using the mobile-first worklight by IBM.',
+      link: '/courses/'
+    },
+    {
+      imgSrc: CNF,
+      title: 'Computer Network Fundamental [CCNA]',
+      description: 'We provide quality training for students to become a Cisco Certified Network Associate (CCNA).',
+      link: '/courses/'
+    },
+  ]
+
   return (
-    <div>
-      <h1>Courses</h1>
-      {/* Add your course content here */}
-    </div>
+    <>
+      <section className="courses">
+      <Header title="Courses" currentPage="Courses" backgroundImage={bg} />
+      <div className="courses-container">
+        <div className="courses-content">
+          <h3>Our Popular Courses</h3>
+          <p>Learn the various techniques used to secure a computer system from attacks through hacking.</p>
+        </div>
+        <div className="courses-cards">
+          {coursesData.map((course, index) => (
+            <div className="course-card wow fadeInUp" key={index}>
+              <div className="course-card-image">
+                <img src={course.imgSrc} alt={course.title} />
+              </div>
+              <div className="course-card-content">
+                <h4>{course.title}</h4>
+                <p>{course.description}</p>
+                <a href={course.link}>Read More</a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+    <Subjects />
+    <Footer />
+    </> 
   );
-};
+}
 
 export default Courses;
